f0f5c36b814b913e1ea2442fcdb199e2db5c9e7b
Cristopher Bohol <cris.bacus6@gmail.com>
2022-08-19 13:14:59 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
