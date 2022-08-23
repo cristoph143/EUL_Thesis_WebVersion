75ead905d72bdb7ea9e5428bc55bea631acba13d
Cristopher Bohol <cris.bacus6@gmail.com>
2022-08-23 15:50:42 +0800

    unhome changes

--- src/app/model/research_details.ts
+++ src/app/model/research_details.ts
@@ -1,2 +1,12 @@
-// export interface ResearchDetails {
-//     id: number;
+export interface ResearchDetails {
+    id: number;
+    category: String[];
+    date_published: Date;
+    adviser: String;
+    authors: String[];
+    title: String;
+    abstract: String;
+    keywords: String[];
+    department: String;
+    qr: String;
+}
