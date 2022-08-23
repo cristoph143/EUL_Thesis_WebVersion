ccf2ab766d695175d1b0812658f279609ba00fe2
Cristopher Bohol <cris.bacus6@gmail.com>
2022-08-23 17:17:46 +0800

    mysql2 expiremental stage.....

--- src/app/backend/index.js
+++ src/app/backend/index.js
@@ -1,80 +1,28 @@
-// connect to database using mysql
-// npm install --save mysql2
-var mysql = require('mysql2');
+const express = require('express');
 
-var con = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'workbench_mysql123',
-    database: 'thesis'
+const bodyParser = require('body-parser');
+
+const groceryRoutes = require('../backend/routes');
+
+const errorController = require('../backend/error');
+
+const app = express();
+
+const ports = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = 'INSERT INTO user (id, name, email, password, department, role) VALUES (1, "John Doe", "john.doe@gmail.com", "password", "IT", "admin"), (2, "Jane Doe", "jane.doe@gmail.com", "password", "IT", "user"), (3, "John Smith", "john.smith@gmail.com", "password", "IT", "teacher")'
-    con.query(sql, function(err, result) {
-        if (err) throw err;
-        console.log("Result: ");
-        // print the result in a json form
-        console.log(JSON.stringify(result));
-    });
-});
+// app.use('/groceries', groceryRoutes);
 
+app.use(errorController.get404);
 
+app.use(errorController.get500);
 
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const events = require('../backend/event.js');
-// const mysql = require('mysql');
-
-// // create db connection
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'workbench_mysql123',
-//     database: 'thesis'
-// });
-
-
-// // db.connect();
-
-// const port = process.env.PORT || 8080;
-
-// const app = express()
-//     .use(cors())
-//     .use(bodyParser.json())
-//     .use(events(connection));
-
-// app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`);
-// }).on('error', err => {
-//     console.log(err);
-// }).on('listening', () => {
-//     console.log(`Server listening on port ${port}`);
-// }).on('close', () => {
-//     console.log(`Server closed on port ${port}`);
-// }).on('exit', () => {
-//     console.log(`Server exited on port ${port}`);
-// }).on('disconnect', () => {
-//     console.log(`Server disconnected on port ${port}`);
-// }).on('reconnect', () => {
-//     console.log(`Server reconnected on port ${port}`);
-// });
-
-// // var mysql = require('mysql-wrapper');
-// // var conn = mysql({
-// //     host: 'localhost',
-// //     port: '3306',
-// //     user: 'root',
-// //     password: 'workbench_mysql123',
-// //     database: 'thesis'
-// // });
-// // conn.query(
-// //     // insert into table user with id, name, email, password, department, and role
-// //     'INSERT INTO users (id, name, email, password, department, role) VALUES (1, "John Doe", "john.doe@gmail.com", "password", "IT", "admin"), (2, "Jane Doe", "jane.doe@gmail.com", "password", "IT", "user"), (3, "John Smith", "john.smith@gmail.com", "password", "IT", "teacher")'
-// // );
+app.listen(ports, () => console.log(`listening on port ${ports}`));
