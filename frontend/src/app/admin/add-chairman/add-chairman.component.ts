6f389d2222707424fbf1e8e6e43db6b122d42567
Cristopher Bohol <cris.bacus6@gmail.com>
2023-01-10 19:34:59 +0800

    add_chairman pt1

--- frontend/src/app/admin/add-chairman/add-chairman.component.ts
+++ frontend/src/app/admin/add-chairman/add-chairman.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-chairman',
@@ -10,6 +11,21 @@ export class AddChairmanComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // <!-- SchoolID, First Name, Last Name, Email, Role, Department, Password, Approve -->
+  form: FormGroup = new FormGroup({
+    schoolID: new FormControl('', Validators.required),
+    first_name: new FormControl('', Validators.required),
+    last_name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    role: new FormControl('', Validators.required),
+    department: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    approve: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
   }
 
 }
